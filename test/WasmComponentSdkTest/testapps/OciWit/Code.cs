9844376388a3157621f88f3e17cc78d97fadf3ec
James Sturtevant <jsturtevant@gmail.com>
2025-03-07 10:39:48 -0800

    Use the identity of the current wit property to save file
    Signed-off-by: James Sturtevant <jsturtevant@gmail.com>

--- test/WasmComponentSdkTest/testapps/OciWit/Code.cs
+++ test/WasmComponentSdkTest/testapps/OciWit/Code.cs
@@ -1,9 +1,18 @@
 using CommandWorld.wit.exports.wasi.cli.v0_2_0;
+using MyFuncsWorld;
 
 public class RunImpl : IRun
 {
     public static void Run()
     {
         Console.WriteLine("Oci is awesome!"); 
+    }
+}
+
+public class MyFuncsWorldImpl : IMyFuncsWorld
+{
+    public static int GetNumber()
+    {
+        return 123;
     }
 }
